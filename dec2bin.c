faaaa359df9316233457edc64ceb2a3ce6c64867
Dongze Li <dol005@ucsd.edu>
2022-01-15 09:32:26 -0800

    edition 1

--- dec2bin.c
+++ dec2bin.c
@@ -1,6 +1,6 @@
 /**
  * Assignment-1
- * CSE30 Username: cs30wi22 (fill out your username)
+ * CSE30 Username: cs30wi22mt
  * Class: UCSD CSE30-WI22
  */
 #include <stdio.h>
@@ -20,6 +20,22 @@ int main(int argc, char* argv[]) {
   
     // put your code here (use as many lines as you need)
 
+    // set up maximum loop and variable needed for the loop
+    int n = 32, i = 0, remain;
+
+    // output to print each line
+    while(i < n){
+        // check for the end of the process
+        if(population == 0)
+            break;
+
+        remain = population % 2;
+        population = population / 2;
+
+        // print the result
+        printf("%d * 2^%d\n", remain, i);
+        i++;
+    }
 
     return EXIT_SUCCESS; 
 }
