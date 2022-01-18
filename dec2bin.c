450b988f8ac59ab657e7238334ba77658293c644
Dongze Li <dol005@ucsd.edu>
2022-01-17 16:34:20 -0800

    Simplify Code

--- dec2bin.c
+++ dec2bin.c
@@ -6,29 +6,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(int argc, char* argv[]) {
-
 
     if (argc != 2) {
         fprintf(stderr, "%s wrong number of arguments\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-
     unsigned int population = (unsigned) atoi(argv[1]);
-  
-    // put your code here (use as many lines as you need)
-
-    // set up maximum loop and variable needed for the loop
-    int n = 32, i = 0, remain;
 
     // output to print each line
-    while(i < n){
-        // check for the end of the process
-        if(population == 0)
-            break;
+    int remain, i = 0;
 
+    while(population > 0){
+        // get the value for variables
         remain = population % 2;
         population = population / 2;
 
